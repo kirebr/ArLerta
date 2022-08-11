0d5eddef98179372f1573e267a09a9bf27c6e14c
kirebr <erik_doko@hotmail.com>
2022-08-11 19:52:11 -0300

    Fix auth

--- src/controllers/AmbienteController.ts
+++ src/controllers/AmbienteController.ts
@@ -1,16 +1,21 @@
 import passport from "passport";
 import AmbienteService from "../services/AmbienteService"
-import { JsonController, UseBefore } from "routing-controllers";
+import { JsonController, UseBefore, Get } from "routing-controllers";
 import { Inject, Service } from "typedi";
 
 
 @JsonController("/ambiente")
-@UseBefore(passport.authenticate("bearer", { session: false }))
+// @UseBefore(passport.authenticate("bearer", { session: false }))
 @Service()
 export default class AmbienteController {
 
   constructor(
     @Inject("ambienteService") private ambienteService: AmbienteService
   ) {}
+
+  @Get()
+  async getAll() {
+    return await (await this.ambienteService.get()).data;
+  }
 
 }
--- src/services/AmbienteService.ts
+++ src/services/AmbienteService.ts
@@ -10,11 +10,6 @@ export default class AmbienteService {
   async get() {
     const response = await axios.post('https://backend-api-floats.vercel.app/api/login', { 'usr': 'inf', 'pass': '25d55ad283aa400af464c76d713c07ad' });
     const { session_token } = response.data;
-    const config = {
-      headers: {
-        session_token
-      }
-    };
-    return await axios.get('https://backend-api-floats.vercel.app/api/ambientes/4', config);
+    return await axios.get('https://backend-api-floats.vercel.app/api/ambientes/4', { headers: { sessiontoken: session_token }});
   }
 }
